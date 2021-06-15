b39437deb2912b155a8f3b4d241b47debf2c6bfd
LeaCrazyBambi <lheynitz@gmail.com>
2021-06-15 16:14:46 +0200

    A start, but I don 't really get it yet

---
+++ index.js
@@ -0,0 +1,17 @@
+// const titel = document.querySelector("h1");
+// console.log(titel);
+// titel.style.color = "red";
+
+// const items = document.querySelectorAll("li");
+// console.log(items);
+// items.forEach((element) => {
+//   element.style.backgroundColor = "purple";
+// });
+
+const bodyElement = document.querySelector("body");
+bodyElement.style.fontFamily = "Sans Serif";
+
+// const mainText = document.querySelector("title");
+// mainText.style.fontFamily = "right";
+
+const headings = document.querySelectorAll("#food-category");
